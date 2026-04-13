bcda10e53c3998365fd572c00d59321d387456c5
HaniNghi <vmmn27@gmail.com>
2026-04-13 14:24:35 +0300

    fix: update implement

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -1,13 +1,44 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from '../screens/HomeScreen';
-import CalculatorScreen from '../screens/CalculatorScreen';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import HomeScreen from "../screens/HomeScreen";
+import CalculatorScreen from "../screens/CalculatorScreen";
+
+import { black, brightBlue, grey } from "../styles";
+
 const Tab = createBottomTabNavigator();
 
 export default function BottomTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+
+        tabBarStyle: {
+          backgroundColor: black,
+          borderTopColor: "#1f2937",
+          height: 70,
+          paddingBottom: 10,
+        },
+
+        tabBarActiveTintColor: brightBlue,
+        tabBarInactiveTintColor: grey,
+
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home";
+          } else if (route.name === "Calculator") {
+            iconName = "calculator";
+          }
+
+          return <Ionicons name={iconName} size={22} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={CalculatorScreen} />
+      <Tab.Screen name="Calculator" component={CalculatorScreen} />
     </Tab.Navigator>
   );
 }

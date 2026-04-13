056a90a950f953bae73c75356ea8b2f5cf38fed8
HaniNghi <vmmn27@gmail.com>
2026-04-13 16:36:56 +0300

    add(services): add getInfo function

--- services/firebase.js
+++ services/firebase.js
@@ -1,7 +1,7 @@
 import { createUserWithEmailAndPassword, updateProfile , signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebaseConfig";
 import { database } from "../firebaseConfig";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, get } from "firebase/database";
 
 export async function createUser(inputUser) {
   try {
@@ -44,4 +44,23 @@ export async function saveInfo(info) {
     goal: info.goal,
   });
   console.log(info)
+}
+
+export async function getInfo(){
+  try {
+    const user = auth.currentUser;
+    if (!user) return null;
+
+    const snapshot = await get(ref(database, `info/${user.uid}`));
+
+    if (snapshot.exists()) {
+      console.log("getInfo ",snapshot.val())
+      return snapshot.val();
+    } else {
+      console.log("No data found");
+      return null;
+    }
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
 }

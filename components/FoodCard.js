0d5053f9a91a9b1999f214ac4d98ddf4da2a8820
HaniNghi <vmmn27@gmail.com>
2026-04-18 10:49:22 +0300

    fix: add and update on delete function

--- components/FoodCard.js
+++ components/FoodCard.js
@@ -1,32 +1,51 @@
-import { StyleSheet, View , Text, TouchableOpacity } from "react-native"
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
-
-
-export default function FoodCard({ name, kcal, unit, onAdd }) {
-    return (
-        <View style={styles.container}>
-            <View style={styles.info}>
-                <Text>{name}</Text>
-                <Text>{kcal} kcal/ 100 {unit}</Text>
-            </View>
-            <TouchableOpacity onPress={onAdd}>
-        <Text style={{ fontSize: 18 }}>＋</Text>
+export default function FoodCard({
+  name,
+  kcal,
+  unit,
+  amount,
+  onAdd,
+  onDelete,
+}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.info}>
+        <Text>{name}</Text>
+        <Text>
+          {kcal} kcal/ 100 {unit}
+        </Text>
+        {amount && (
+          <Text>
+            Amount: {amount} {unit}
+          </Text>
+        )}
+      </View>
+      {onAdd && (
+        <TouchableOpacity onPress={onAdd}>
+          <Text style={{ fontSize: 18 }}>＋</Text>
+        </TouchableOpacity>
+      )}
+      {onDelete && (
+        <TouchableOpacity onPress={onDelete}>
+        <Text style={{ fontSize: 18 }}>-</Text>
       </TouchableOpacity>
-        </View>
-    )
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent:"space-between",
-        marginLeft: 20,
-        marginRight: 20,
-        marginTop: 5,
-    },
-    info: {
-        display: "flex",
-        flexDirection: "column",
-    }
-})
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 5,
+  },
+  info: {
+    display: "flex",
+    flexDirection: "column",
+  },
+});

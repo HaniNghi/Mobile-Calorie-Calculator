6b5c8ac555083eed84db8c476f5dd08d5b1bdf7f
HaniNghi <vmmn27@gmail.com>
2026-04-24 11:19:03 +0300

    add: add InfoForm and fetch info from firebase

--- screens/Info.js
+++ screens/Info.js
@@ -1,17 +1,55 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, Button, TouchableOpacity, StyleSheet } from "react-native";
+import {View, Text, Button, TouchableOpacity, StyleSheet } from "react-native";
 // Colors
 import { black, grey, white, brightBlue } from "../styles";
 import { logout } from "../services/firebase";
+import InfoForm from "../components/InfoForm";
+import { useEffect, useState } from "react";
+import { getInfo } from "../services/firebase";
 
 export default function Info() {
   const handleLogout = async () => {
     await logout();
   };
+  const [info, setInfo] = useState({
+    age: null,
+    gender: null,
+    height: null,
+    weight: null,
+    activityLevel: null,
+    goal: null,
+  });
+
+  const fetchInfo = async () => {
+    try {
+    const infoFromFirebase = await getInfo();
+
+    if(infoFromFirebase){
+      setInfo(infoFromFirebase)
+    }
+    } catch(error) {
+      console.log(error);
+    }
+    
+    
+  };
+
+  const handleSave = () =>{
+    console.log("saved")
+  }
+
+  useEffect(()=>{
+    fetchInfo()
+  },[])
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: black }}>
       <Text style={{ color: white }}>Your Info</Text>
+      <InfoForm
+        info={info}
+        handleSave={handleSave}
+        buttonText={"Save changes"}
+      />
       <TouchableOpacity style={styles.saveBtn} onPress={handleLogout}>
         <Text style={styles.saveText}>Log out</Text>
       </TouchableOpacity>
